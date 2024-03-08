10ef43a8c015ea859415a80efe5692928176f364
DAN1Ss <104832269+DAN1Ss@users.noreply.github.com>
2024-03-08 16:13:30 +0000

    TESTE

---
+++ src/main/java/LineSchedule.java
@@ -0,0 +1,10 @@
+public class LineSchedule {
+
+    public LineSchedule(String curso, String unidade_curricular, String turno, String turma; Integer inscritos, String dia_semana, String hora_inicio, String hora_fim,  String data_aula;Características da sala pedida para a aula;Sala atribuída à aula) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
