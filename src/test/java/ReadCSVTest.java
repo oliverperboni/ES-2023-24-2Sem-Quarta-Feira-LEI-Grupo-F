c53ed030bf945ab36ed6e66a5f2543388a815ec8
DAN1Ss <104832269+DAN1Ss@users.noreply.github.com>
2024-03-16 12:07:57 +0000

    Foi criada a class de testes CSVFileChooserTest para testar as funções da class CSVFileChooser. O teste funcionou. Retirei um pouco de código do teste do ReadCSVTest que não estava a fazer o suposto. Assim o teste já funciona

--- src/test/java/ReadCSVTest.java
+++ src/test/java/ReadCSVTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -7,23 +9,17 @@ import java.util.ArrayList;
 class ReadCSVTest {
 
     @Test
-    void readScheduleCSV() {
-        // Caminho para o arquivo CSV de teste
-        String csvFile = "csv/HorarioDeExemplo.csv"; 
+    void testReadScheduleCSV() {
+        String csvFile = "csv/HorarioDeExemplo.csv";
 
-        // ME;Teoria dos Jogos e dos Contratos;01789TP01;MEA1;30;Sex;13:00:00;14:30:00;02/12/2022;Sala Aulas Mestrado;AA2.25
-
-
-        // Chama o método readScheduleCSV
+        //Chama o método readScheduleCSV
         ArrayList<LineSchedule> result = ReadCSV.readScheduleCSV(csvFile);
 
-        // Verificações
-
-        // Verifica se a lista resultante não é nula
+        //Verifica se a lista resultante não é nula
         assertNotNull(result);
 
-        // Verifica se o número de objetos LineSchedule criados está correto
-        assertEquals(26019, result.size()); // Substitua pelo número real de linhas no arquivo
+        //Verifica se o número de objetos LineSchedule criados está correto
+        assertEquals(26019, result.size()); //Número real de linhas no arquivo no primeiro argumento
 
         // Verifica se os objetos LineSchedule foram criados corretamente
         LineSchedule firstSchedule = result.get(0);
